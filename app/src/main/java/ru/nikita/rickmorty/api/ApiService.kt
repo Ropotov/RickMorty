41cfc840541d18d0308293b20ab5b14f24d4fc62
Nikita <ropotov.dev@gmail.com>
2022-09-30 17:55:01 +0400

    -разбираемся с пагинацией

--- app/src/main/java/ru/nikita/rickmorty/api/ApiService.kt
+++ app/src/main/java/ru/nikita/rickmorty/api/ApiService.kt
@@ -2,9 +2,13 @@ package ru.nikita.rickmorty.api
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 import ru.nikita.rickmorty.data.Character
 
 interface ApiService {
     @GET("character/")
-    suspend fun getCharacter(): Response<Character>
+    suspend fun getCharacter(
+        @Query("page") page: Int = 1,
+        @Query("pageSize") pageSize: Int = 20
+    ): Response<Character>
 }
