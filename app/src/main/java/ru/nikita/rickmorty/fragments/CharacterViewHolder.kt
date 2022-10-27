84d9a2e80d0dfd83bebd2727b9ae5f7d60c29ab7
Nikita <ropotov.dev@gmail.com>
2022-10-27 21:15:49 +0400

    ViewHolder

---
+++ app/src/main/java/ru/nikita/rickmorty/fragments/CharacterViewHolder.kt
@@ -0,0 +1,31 @@
+package ru.nikita.rickmorty.fragments
+
+import android.graphics.Color
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import ru.nikita.rickmorty.data.Result
+import ru.nikita.rickmorty.databinding.ItemCharacterBinding
+import ru.nikita.rickmorty.utils.CharacterStatusEnums
+
+class CharacterViewHolder(private val binding: ItemCharacterBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(position: Result) {
+        with(binding) {
+            tvNameCharacter.text = position.name
+            tvCharacterRace.text = position.species
+            tvLastLocation.text = position.location.name
+            when (position.status) {
+                CharacterStatusEnums.CHARACTER_ALIVE.value ->
+                    tvStatusColor.setBackgroundColor(Color.parseColor("#02B31A"))
+                CharacterStatusEnums.CHARACTER_DEAD.value ->
+                    tvStatusColor.setBackgroundColor(Color.parseColor("#FF0800"))
+                else ->
+                    tvStatusColor.setBackgroundColor(Color.parseColor("#E3E3E3"))
+            }
+            Glide
+                .with(ivCharacterPhoto)
+                .load(position.image)
+                .into(ivCharacterPhoto)
+        }
+    }
+}
