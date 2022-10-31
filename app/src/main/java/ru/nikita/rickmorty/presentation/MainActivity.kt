d56d8a67496171995c7ee615c6cbb8dd08179caa
Nikita <ropotov.dev@gmail.com>
2022-10-31 17:40:23 +0400

    splash screen

--- app/src/main/java/ru/nikita/rickmorty/presentation/MainActivity.kt
+++ app/src/main/java/ru/nikita/rickmorty/presentation/MainActivity.kt
@@ -1,13 +1,24 @@
 package ru.nikita.rickmorty.presentation
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ru.nikita.rickmorty.R
 import ru.nikita.rickmorty.presentation.fragments.CharacterListFragment
+import ru.nikita.rickmorty.presentation.fragments.CharacterListViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: CharacterListViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.characterLD.value != null
+            }
+        }
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
